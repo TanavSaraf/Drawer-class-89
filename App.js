7ea4bae89cf6733a0aed644ea366c70a2bf13e92
TANAV <tanavsaraf@gmail.com>
2021-09-06 19:07:51 +0530

    hello

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import DrawerFunction from "./components/drawerNav";
+
+import { createStackNavigator } from "@react-navigation/stack";
+import Dashboard from "./screens/dashboard";
+import Loading from "./screens/loading";
+import Login from "./screens/login";
+import firebase from "firebase";
+import {firebaseConfig} from './config';
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig)
+}else{
+  firebase.app()
+}
+const StackNav = createStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
-      <DrawerFunction/>
+      <StackNav.Navigator screenOptions={{headerShown:false}}>
+        <StackNav.Screen name="Loading" component={Loading} />
+        <StackNav.Screen name="Login" component={Login} />
+        <StackNav.Screen name="DashBoard" component={Dashboard} />
+      </StackNav.Navigator>
     </NavigationContainer>
   );
 }
